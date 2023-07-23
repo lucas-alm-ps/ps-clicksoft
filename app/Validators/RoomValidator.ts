b07049b593dce6f0ba1d5d34d59959d4bf703031
me.luc <lukasalmeida372@gmail.com>
2023-07-23 01:20:32 -0300

    feat: add request validators

---
+++ app/Validators/RoomValidator.ts
@@ -0,0 +1,26 @@
+import { schema, CustomMessages } from "@ioc:Adonis/Core/Validator";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+export default class RoomValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  get validateAll() {
+    return true;
+  }
+
+  public async fails(errorMessages: any[]) {
+    return this.ctx.response.status(400).json(errorMessages);
+  }
+
+  public schema = schema.create({
+    number: schema.number(),
+    capacity: schema.number(),
+    available: schema.boolean(),
+  });
+
+  public messages: CustomMessages = {
+    "number.required": "Campo name é obrigatório",
+    "capacity.required": "Campo email é obrigatório",
+    "available.required": "Campo enrollment é obrigatório",
+  };
+}
---
+++ app/Validators/StudentValidator.ts
@@ -0,0 +1,28 @@
+import { schema, CustomMessages } from "@ioc:Adonis/Core/Validator";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+export default class StudentValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  get validateAll() {
+    return true;
+  }
+
+  public async fails(errorMessages: any[]) {
+    return this.ctx.response.status(400).json(errorMessages);
+  }
+
+  public schema = schema.create({
+    name: schema.string(),
+    email: schema.string(),
+    enrollment: schema.string(),
+    birthDate: schema.date({ format: "dd/MM/yyyy" }),
+  });
+
+  public messages: CustomMessages = {
+    "name.required": "Campo name é obrigatório",
+    "email.required": "Campo email é obrigatório",
+    "enrollment.required": "Campo enrollment é obrigatório",
+    "birthDate.required": "Campo birthDate é obrigatório",
+  };
+}
