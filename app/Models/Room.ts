1c77b0d93b539adc9093ec48ed13aebe5a8aad0c
me.luc <lukasalmeida372@gmail.com>
2023-07-22 23:11:16 -0300

    feat: add room model

---
+++ app/Models/Room.ts
@@ -0,0 +1,25 @@
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+
+export default class Room extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public number: number;
+
+  @column()
+  public capacity: number;
+
+  @column()
+  public available: boolean;
+
+  @column()
+  public description: string;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+}
